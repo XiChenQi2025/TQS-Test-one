46c4609c873430d49152f951b49bb31db4757cde
XiChenQi2025 <y1519201066@gmail.com>
2025-12-06 13:27:41 +0800

    Delete router directory

--- router/index.js
+++
@@ -1,7 +0,0 @@
-import Router from './Router.js';
-import { routes } from '../config/routes.js';
-
-const router = new Router();
-routes.forEach(route => router.addRoute(route.path, route));
-
-export default router;
